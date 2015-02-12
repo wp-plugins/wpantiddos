3187cf2f4405de0d4622f4331b8973144fff43a2
zzmaster <zzmaster@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-12 07:10:10 +0000

    One more first commit
    git-svn-id: https://plugins.svn.wordpress.org/wpantiddos/trunk@1087886 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- 2.0/uninstall.php
+++
@@ -1,8 +0,0 @@
-<?
-	//if uninstall not called from WordPress exit
-	if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) 
-		exit();
-
-    require_once('Wpantiddos_Plugin.php');
-    $wp_antiddos_plugin = new Wpantiddos_Plugin();
-	$wp_antiddos_plugin->uninstall();
